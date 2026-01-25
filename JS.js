bd0ccde0e09c0434163ef712d7bceffa6482cbb7
Doryan <doryan.berg@etudiant.univ-rennes.fr>
2026-01-25 01:03:11 +0100

    add Finch

--- JS.js
+++ JS.js
@@ -32,6 +32,13 @@ const amiibo = document.getElementById("amiibo");
 /* Démarre le JSON pour l'application automatisé des images */
 const data = [
   {
+    "image": "images/img_montage/Chinf.png",
+    "alt": "alt Finch - Xenoblade Chronicles 2",
+    "class": "",
+    "titre": "Chaette",
+    "description": ""
+  },
+  {
     "image": "images/img_fond/Chaette.png",
     "alt": "alt chouette - Carimara",
     "class": "",
